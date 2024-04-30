ed5003dc31681996bd8693ed7d4c5928a2019cd4
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-30 02:49:00 -0500

    Add prettier/eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import GeneralInfo from "./components/GeneralInfo/GeneralInfo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <GeneralInfo />
